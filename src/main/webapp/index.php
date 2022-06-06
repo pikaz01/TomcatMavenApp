857db9e81ef4214fc3d382e4a95123dfb24e69f4
pikaz01 <92135241+pikaz01@users.noreply.github.com>
2022-06-06 20:22:36 +0530

    Update index.php

--- src/main/webapp/index.php
+++ src/main/webapp/index.php
@@ -1,3 +1,10 @@
-<?php
-echo "asasasa"
-?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>TomcatMavenApp</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    </head>
+    <body>
+        <h1>Welcome to Realtekh Home Page!</h1>
+    </body>
+</html>
